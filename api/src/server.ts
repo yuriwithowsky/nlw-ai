1fb0d26d4fc008cbf1e154fa89f0cd4a47b513e0
Yuri Withowsky <yuri.jivago14@gmail.com>
2023-09-13 00:28:10 -0300

    aula 2

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,20 @@
 import { fastify } from "fastify";
+import { getAllPromptsRoute } from "./routes/get-all-prompts";
+import { uploadVideoRoute } from "./routes/upload-video";
+import { createTranscriptionRoute } from "./routes/create-transcription";
+import { generateAICompletionRoute } from "./routes/generate-ai-completion";
+import { fastifyCors } from "@fastify/cors";
 
 const app = fastify();
 
-app.get("/", () => {
-  return "hello world";
+app.register(fastifyCors, {
+  origin: "*",
 });
+
+app.register(getAllPromptsRoute);
+app.register(uploadVideoRoute);
+app.register(createTranscriptionRoute);
+app.register(generateAICompletionRoute);
 
 app
   .listen({
